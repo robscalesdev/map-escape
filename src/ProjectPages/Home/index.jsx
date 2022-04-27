fab656886fd3d722284cd4529a09269da81ab903
rob <imafishyfriend@gmail.com>
2022-04-27 17:27:51 -0500

    updated language on home page

--- src/ProjectPages/Home/index.jsx
+++ src/ProjectPages/Home/index.jsx
@@ -17,24 +17,24 @@ const Home = () => {
     <HomeContainer>
         <ExplanationContainer>
             <ExplanationH1>How To Play</ExplanationH1>
-            <ExplanationP>The evil dot is trying to escape, your goal is to place walls to keep him from getting away.</ExplanationP>
+            <ExplanationP>The chicken is trying to escape, your goal is to place walls to keep him from getting away.</ExplanationP>
             <ExplanationP>Each turn you will be able to place one wall, then he will get to move one tile. Can you capture him before it's too late?</ExplanationP>
         </ExplanationContainer>
         <GameContainer>
             <GameCard>
                 <CardTitle>Surround</CardTitle>
                 <CardImage></CardImage>
-                <CardDesc>Don't let the evil dot reach the edges.</CardDesc>
+                <CardDesc>Don't let the chicken reach the edges.</CardDesc>
             </GameCard>
             <GameCard>
                 <CardTitle>Trap</CardTitle>
                 <CardImage></CardImage>
-                <CardDesc>Place walls until the dot has no moves remaining.</CardDesc>
+                <CardDesc>Place walls until the chicken has no moves remaining.</CardDesc>
             </GameCard>
             <GameCard>
                 <CardTitle>About</CardTitle>
                 <CardImage></CardImage>
-                <CardDesc>Learn more about the logic the dot uses to select where to go.</CardDesc>
+                <CardDesc>Learn more about the logic the chicken uses to select where to go.</CardDesc>
             </GameCard>
         </GameContainer>
     </HomeContainer>
