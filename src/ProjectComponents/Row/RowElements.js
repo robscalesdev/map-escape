685b025a346fac2d8a8f286e48c47b8d41d90660
rob <imafishyfriend@gmail.com>
2022-04-27 18:45:13 -0500

    added styling to board

--- src/ProjectComponents/Row/RowElements.js
+++ src/ProjectComponents/Row/RowElements.js
@@ -3,15 +3,17 @@ import styled from 'styled-components'
 export const Tile = styled.div`
   width: 75px;
   height: 75px;
+  margin: 2px;
   border: solid;
+  border-radius: 10%;
   display: flex;
   justify-content: center;
   align-items: center;
   background: #8ed860;
 
   /* background: ${({ isWall }) => (isWall ? '#C19A6B' : '#7ec850' )}; */
-  font-size: ${({ isWall, isChar }) => (isWall || isChar ? '4rem' : '#fff' )};
-  color: ${({ isWall, isChar }) => (isWall ? '#a17A4B' : (isChar ? 'red' : '#222'))};
+  font-size: ${({ isWall, isChar }) => (isWall || isChar ? '4rem' : '1rem' )};
+  color: ${({ isWall, isChar }) => (isWall ? '#a17A4B' : (isChar ? '#d00' : '#222'))};
   border-color: #5ea830;
 
   @media screen and (max-width: 700px) {
@@ -22,5 +24,6 @@ export const Tile = styled.div`
   @media screen and (max-width: 650px) {
     width: 45px;
     height: 45px;
+    margin: 1px;
   }
 `
