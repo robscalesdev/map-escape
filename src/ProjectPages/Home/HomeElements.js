3e7b9eb321a0b01ae18f8fac3df16a44def54ca9
rob <imafishyfriend@gmail.com>
2022-04-27 17:43:00 -0500

    updated home page

--- src/ProjectPages/Home/HomeElements.js
+++ src/ProjectPages/Home/HomeElements.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-export const HomeContainer = styled.div``
+export const HomeContainer = styled.div`
+  margin: 2%;
+`
 
 export const ExplanationContainer = styled.div``
 
@@ -10,7 +13,25 @@ export const ExplanationP = styled.p``
 
 export const GameContainer = styled.div``
 
-export const GameCard = styled.div``
+export const GameCard = styled.div`
+  margin: 2% 0;
+  padding: 2%;
+
+  border: solid;
+  border-width: 2px;
+  border-radius: 5px;
+  border-color: #777;
+
+  cursor: pointer;
+`
+
+export const CardLink = styled(Link)`
+  text-decoration: none;
+
+  &:visited {
+    color: black;
+  }
+`
 
 export const CardTitle = styled.h2``
 
--- src/ProjectPages/Home/index.jsx
+++ src/ProjectPages/Home/index.jsx
@@ -4,7 +4,8 @@ import {
     HomeContainer,
     ExplanationContainer,
     GameContainer,
-    GameCard, 
+    GameCard,
+    CardLink,
     ExplanationH1,
     ExplanationP,
     CardTitle,
@@ -17,24 +18,30 @@ const Home = () => {
     <HomeContainer>
         <ExplanationContainer>
             <ExplanationH1>How To Play</ExplanationH1>
-            <ExplanationP>The chicken is trying to escape, your goal is to place walls to keep him from getting away.</ExplanationP>
-            <ExplanationP>Each turn you will be able to place one wall, then he will get to move one tile. Can you capture him before it's too late?</ExplanationP>
+            <ExplanationP>The chicken is trying to escape, your goal is to place walls to keep it from getting away.</ExplanationP>
+            <ExplanationP>Each turn you will be able to place one wall, then it will get to move one tile. Can you capture it before it's too late?</ExplanationP>
         </ExplanationContainer>
         <GameContainer>
             <GameCard>
-                <CardTitle>Surround</CardTitle>
-                <CardImage></CardImage>
-                <CardDesc>Don't let the chicken reach the edges.</CardDesc>
+                <CardLink to='/surround'>
+                    <CardTitle>Surround</CardTitle>
+                    <CardImage></CardImage>
+                    <CardDesc>Don't let the chicken reach the edges.</CardDesc>
+                </CardLink>
             </GameCard>
             <GameCard>
-                <CardTitle>Trap</CardTitle>
-                <CardImage></CardImage>
-                <CardDesc>Place walls until the chicken has no moves remaining.</CardDesc>
+                <CardLink to='/trap'>
+                    <CardTitle>Trap</CardTitle>
+                    <CardImage></CardImage>
+                    <CardDesc>Place walls until the chicken has no moves remaining.</CardDesc>
+                </CardLink>
             </GameCard>
             <GameCard>
-                <CardTitle>About</CardTitle>
-                <CardImage></CardImage>
-                <CardDesc>Learn more about the logic the chicken uses to select where to go.</CardDesc>
+                <CardLink to='/about'>
+                    <CardTitle>About</CardTitle>
+                    <CardImage></CardImage>
+                    <CardDesc>Learn more about the logic the chicken uses to select where to go.</CardDesc>
+                </CardLink>
             </GameCard>
         </GameContainer>
     </HomeContainer>
